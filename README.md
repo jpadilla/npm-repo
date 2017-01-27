7a3c117cfb84bdd4b00710f755d499ad3f064689
José Padilla <jpadilla@webapplicate.com>
2017-01-27 01:02:41 -0500

    Add a homepage

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Redirect to an npm package's repository
 
 I got tired of going to `https://www.npmjs.com/package/<PACKAGE-NAME>`, browsing and clicking the repository URL.
 
-![npm-repo](https://cloud.githubusercontent.com/assets/83319/22361308/5a289450-e426-11e6-9dc6-a74234ceaace.gif)
+![npm-repo](https://cloud.githubusercontent.com/assets/83319/22361889/223deeb8-e42c-11e6-896a-1cb5bdb38b20.gif)
--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const urlParse = require('url').parse;
+const micro = require('micro');
 const fetch = require('node-fetch');
 const hostedGitInfo = require('hosted-git-info');
-const { send, createError } = require('micro');
+const pkg = require('./package.json');
+
+const pickRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const POPULAR = ['lodash', 'request', 'express', 'npm', 'debug'];
+const EXAMPLE_URL = `${pkg.homepage}/${pickRandom(POPULAR)}`;
+const README = (
+  '<!doctype html><meta charset="utf-8">' +
+  `<title>${pkg.description}</title>` +
+  `<strong>🚀 ${pkg.description}</strong>` +
+  `<p>Example: <a href="${EXAMPLE_URL}">${EXAMPLE_URL}</a></p>` +
+  '<p>Made with ❤️ by <a href="http://jpadilla.com">Jose Padilla</a><br>' +
+  `Source: <a href="${pkg.repository}">${pkg.repository}</a></p>`
+);
 
 function unknownHostedUrl(reporUrl) {
   // From https://github.com/npm/npm/blob/master/lib/repo.js
@@ -36,9 +50,17 @@ async function getPackage(name) {
   return response.json();
 }
 
-function getRepoUrl(url) {
-  const info = hostedGitInfo.fromUrl(url);
-  const browseUrl = info ? info.browse() : unknownHostedUrl(url);
+async function getPackageRepoUrl(name) {
+  const response = await getPackage(name);
+  const repository = response.repository || {};
+  const repoUrl = repository.url;
+
+  if (!repoUrl) {
+    throw new Error('No repository URL found.');
+  }
+
+  const info = hostedGitInfo.fromUrl(repoUrl);
+  const browseUrl = info ? info.browse() : unknownHostedUrl(repoUrl);
 
   if (!browseUrl) {
     throw new Error('Could not get repository url');
@@ -47,32 +69,20 @@ function getRepoUrl(url) {
   return browseUrl;
 }
 
-function getPackageRepoUrl(pkg) {
-  const repository = pkg.repository || {};
-  const repoUrl = repository.url;
-
-  if (!repoUrl) {
-    throw new Error('No repository URL found.');
-  }
-
-  return getRepoUrl(repoUrl);
-}
-
 module.exports = async function(req, res) {
   let repositoryUrl;
   const pkgName = getPackageName(req.url);
 
   if (!pkgName) {
-    throw createError(404, 'Invalid package name');
+    return micro.send(res, 200, README);
   }
 
   try {
-    const pkg = await getPackage(pkgName);
-    repositoryUrl = getPackageRepoUrl(pkg);
+    repositoryUrl = await getPackageRepoUrl(pkgName);
   } catch (err) {
     repositoryUrl = `https://www.npmjs.com/package/${pkgName}`;
   }
 
   res.setHeader('Location', repositoryUrl);
-  return send(res, 302);
+  return micro.send(res, 302);
 };
